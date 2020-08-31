322ad4fa461a3bd2a354662c0dd753bcfd045ae6
Janita M <jaymadr@gmail.com>
2020-08-31 16:46:06 -0400

    display idea to edit in modal

--- client/src/components/ideas/EditIdeaModal.js
+++ client/src/components/ideas/EditIdeaModal.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Button, makeStyles, TextField, Typography } from '@material-ui/core';
+import {
+  Button,
+  makeStyles,
+  TextField,
+  Typography,
+  Input
+} from '@material-ui/core';
+import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 const useStyles = makeStyles({
   container: {
@@ -14,6 +22,28 @@ const useStyles = makeStyles({
   },
   m1: {
     margin: '1rem'
+  },
+  uploadContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: '1rem'
+  },
+  uploadInput: {
+    display: 'none'
+  },
+  uploadBtn: {
+    backgroundColor: 'red',
+    marginLeft: '1rem'
+  },
+  image: {
+    textAlign: 'center',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    marginBottom: '1rem',
+    width: '15vw'
   }
 });
 
@@ -22,12 +52,24 @@ export default function EditIdeaModal({ ideaToEdit }) {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
 
+  const handleCost = (e, newCost) => {
+    setUpdatedInfo({ ...updatedInfo, cost: newCost });
+  };
+
+  const handleIndoorOutdoor = (e, newIndoorOutdoor) => {
+    setUpdatedInfo({ ...updatedInfo, indoor_outdoor: newIndoorOutdoor });
+  };
+
+  const handleWeather = (e, newWeather) => {
+    setUpdatedInfo({ ...updatedInfo, weather: newWeather });
+  };
+
   const [updatedInfo, setUpdatedInfo] = useState({
     title: '',
     location: '',
     description: '',
     cost: '',
-    indoorOutdoor: '',
+    indoor_outdoor: '',
     category: '',
     url: '',
     picture: undefined,
@@ -42,8 +84,8 @@ export default function EditIdeaModal({ ideaToEdit }) {
     location,
     description,
     cost,
-    indoorOutdoor,
-    cateogry,
+    indoor_outdoor,
+    category,
     url,
     picture,
     convertIdeaPic,
@@ -52,6 +94,7 @@ export default function EditIdeaModal({ ideaToEdit }) {
   } = updatedInfo;
 
   const onChange = (e) => {
+    e.preventDefault();
     setUpdatedInfo({ ...updatedInfo, [e.target.name]: e.target.value });
   };
 
@@ -64,25 +107,21 @@ export default function EditIdeaModal({ ideaToEdit }) {
   };
 
   useEffect(() => {
-    if (ideaToEdit !== null) {
-      setUpdatedInfo(ideaToEdit);
-    } else {
-      setUpdatedInfo({
-        title: '',
-        location: '',
-        description: '',
-        cost: '',
-        indoorOutdoor: '',
-        category: '',
-        url: '',
-        picture: undefined,
-        convertIdeaPic: '',
-        weather: '',
-        isCompleted: false
-      });
-    }
+    if (ideaToEdit) setUpdatedInfo(ideaToEdit);
   }, [ideaToEdit]);
 
+  const handlePicture = (e) => {
+    setUpdatedInfo({
+      ...updatedInfo,
+      convertIdeaPic: URL.createObjectURL(e.target.files[0]),
+      picture: e.target.files[0]
+    });
+  };
+
+  const updateIdea = (e) => {
+    e.preventDefault();
+    console.log('update idea');
+  };
   return (
     <div className={classes.container}>
       <Typography variant='h5' component='h1' gutterBottom>
@@ -126,19 +165,128 @@ export default function EditIdeaModal({ ideaToEdit }) {
           onChange={onChange}
           className={classes.m1}
         />
+
+        <ToggleButtonGroup
+          value={cost}
+          name='cost'
+          exclusive
+          onChange={handleCost}
+          aria-label='cost'
+        >
+          <ToggleButton value='cheap' aria-label='cheap cost'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/cheap-2.png'
+              alt='cheap cost'
+            />
+          </ToggleButton>
+          <ToggleButton value='average' aria-label='average cost'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/average-2.png'
+              alt='average cost'
+            />
+          </ToggleButton>
+          <ToggleButton value='expensive' aria-label='expensive cost'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/expensive.png'
+              alt='expenisve cost'
+            />
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <ToggleButtonGroup
+          name='indoor_outdoor'
+          value={indoor_outdoor}
+          exclusive
+          onChange={handleIndoorOutdoor}
+          aria-label='indoors or outdoors'
+        >
+          <ToggleButton value='outdoor' aria-label='outdoor'>
+            <img
+              src='https://img.icons8.com/doodle/48/000000/coniferous-tree.png'
+              alt='outdoor'
+            />
+          </ToggleButton>
+          <ToggleButton value='indoor' aria-label='indoor'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/home.png'
+              alt='indoor'
+            />
+          </ToggleButton>
+        </ToggleButtonGroup>
+
+        <ToggleButtonGroup
+          name='weather'
+          value={weather}
+          exclusive
+          onChange={handleWeather}
+          aria-label='weather'
+        >
+          <ToggleButton value='sunny' aria-label='sunny'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/summer.png'
+              alt='sunny'
+            />
+          </ToggleButton>
+          <ToggleButton value='rain' aria-label='rain'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/rain.png'
+              alt='rain'
+            />
+          </ToggleButton>
+          <ToggleButton value='snow' aria-label='snow'>
+            <img
+              src='https://img.icons8.com/dusk/64/000000/snow-storm.png'
+              alt='snow'
+            />
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <TextField
+          variant='outlined'
+          label='Give it a category/tag'
+          type='text'
+          name='category'
+          value={category}
+          onChange={onChange}
+          className={classes.m1}
+        />
+
+        <div className={classes.uploadContainer}>
+          <img
+            className={classes.image}
+            src={
+              updatedInfo.convertIdeaPic
+                ? updatedInfo.convertIdeaPic
+                : 'https://img.icons8.com/plasticine/100/000000/image.png'
+            }
+            alt='user profile picture'
+          />
+          <Input
+            accept='image/*'
+            className={classes.uploadInput}
+            id='upload-btn'
+            multiple
+            type='file'
+            name='picture'
+            label='picture'
+            onChange={(e) => handlePicture(e)}
+          />
+          <label htmlFor='upload-btn'>
+            <Button
+              variant='contained'
+              className={classes.uploadBtn}
+              component='span'
+              startIcon={<PhotoCamera />}
+            >
+              Upload
+            </Button>
+          </label>
+        </div>
       </form>
+      <div>
+        <Button variant='outlined' color='primary' onClick={updateIdea}>
+          Update
+        </Button>
+      </div>
     </div>
-    // <div>
-    //   <Typography variant='subtitle1'>Selected: {selectedValue}</Typography>
-    //   <br />
-    //   <Button variant='outlined' color='primary' onClick={handleClickOpen}>
-    //     Open simple dialog
-    //   </Button>
-    //   <SimpleDialog
-    //     selectedValue={selectedValue}
-    //     open={open}
-    //     onClose={handleClose}
-    //   />
-    // </div>
   );
 }
